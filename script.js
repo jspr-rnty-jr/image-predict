f961a3f8070b1809b987c3565b4ac77333c94ac2
jspr-rnty-jr <jasperjr@rantay@gmail.com>
2024-08-11 01:11:22 +0800

    add image predict using image upload; preview

--- script.js
+++ script.js
@@ -4,8 +4,10 @@
     // the link to your model provided by Teachable Machine export panel
     const URL = "https://teachablemachine.withgoogle.com/models/NkH6nvLXA/";
 
-    let model, webcam, labelContainer, maxPredictions;
+    let model, labelContainer, maxPredictions;
+    let img = document.getElementById('image-preview');
 
+    
     // Load the image model and setup the webcam
     async function init() {
         const modelURL = URL + "model.json";
@@ -18,34 +20,45 @@
         model = await tmImage.load(modelURL, metadataURL);
         maxPredictions = model.getTotalClasses();
 
-        // Convenience function to setup a webcam
-        const flip = true; // whether to flip the webcam
-        webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
-        await webcam.setup(); // request access to the webcam
-        await webcam.play();
-        window.requestAnimationFrame(loop);
-
-        // append elements to the DOM
-        document.getElementById("webcam-container").appendChild(webcam.canvas);
         labelContainer = document.getElementById("label-container");
         for (let i = 0; i < maxPredictions; i++) { // and class labels
             labelContainer.appendChild(document.createElement("div"));
         }
-    }
-
-    async function loop() {
-        webcam.update(); // update the webcam frame
-        await predict();
-        window.requestAnimationFrame(loop);
+        
+        await predict()
     }
 
     // run the webcam image through the image model
     async function predict() {
         // predict can take in an image, video or canvas html element
-        const prediction = await model.predict(webcam.canvas);
+        const prediction = await model.predict(img);
         for (let i = 0; i < maxPredictions; i++) {
             const classPrediction =
                 prediction[i].className + ": " + prediction[i].probability.toFixed(2);
             labelContainer.childNodes[i].innerHTML = classPrediction;
         }
     }
+
+    // Upload image logic
+    const fileInput = document.getElementById('img');
+
+    const uploadFile = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                img.src = e.target.result;
+            }
+            
+            reader.readAsDataURL(file);
+        }
+
+        
+        if(labelContainer){
+            while (labelContainer.hasChildNodes()) {
+                labelContainer.removeChild(labelContainer.firstChild);
+            }
+        }
+    }
+
+    fileInput.addEventListener('change', uploadFile);
